32f6b71da01625ff0ba3e8d70d2f918598d1371b
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-17 16:21:19 -0700

    add back of card

--- src/components/canvas/playing-card/PlayingCard.tsx
+++ src/components/canvas/playing-card/PlayingCard.tsx
@@ -4,15 +4,27 @@ import { RoundedRect } from '../RoundedRect';
 const ASPECT_RATIO = 655 / 930;
 
 export const PlayingCard = () => {
-  const texture = useTexture('textures/playing-cards/Hearts_A_white.png');
+  const [frontTexture, backTexture] = useTexture([
+    'textures/kenney/cardClubsA.png',
+    'textures/kenney/cardBack_red2.png',
+  ]);
   const height = 2;
   const width = height * ASPECT_RATIO;
-  const radius = width * 0.1;
+  const radius = width * 0.05;
   return (
     <>
-      <RoundedRect width={width} height={height} radius={radius}>
-        <meshBasicMaterial map={texture} />
-      </RoundedRect>
+      <object3D>
+        <object3D>
+          <RoundedRect width={width} height={height} radius={radius}>
+            <meshBasicMaterial map={frontTexture} />
+          </RoundedRect>
+        </object3D>
+        <object3D rotation-y={Math.PI}>
+          <RoundedRect width={width} height={height} radius={radius}>
+            <meshBasicMaterial map={backTexture} />
+          </RoundedRect>
+        </object3D>
+      </object3D>
     </>
   );
 };
