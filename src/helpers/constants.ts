90cc21f4596cbd941390ac9b38bf3e60a8e2b3c8
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-17 20:53:52 -0700

    add background color

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,3 +1,5 @@
 export const CARD_WIDTH = 63.5e-3; // 63.5mm
 export const CARD_HEIGHT = 88.9e-3; // 88.9mm
 export const CARD_ASPECT_RATIO = CARD_WIDTH / CARD_HEIGHT;
+
+export const BACKGROUND_COLOR = '#03C03C';
