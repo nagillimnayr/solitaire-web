38346d1f39dbd818ffa804bba328dcbad492cad6
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-17 20:06:43 -0700

    add constants

--- src/components/canvas/playing-card/PlayingCard.tsx
+++ src/components/canvas/playing-card/PlayingCard.tsx
@@ -1,26 +1,24 @@
 import { useTexture } from '@react-three/drei';
 import { RoundedRect } from '../RoundedRect';
-
-const ASPECT_RATIO = 655 / 930;
+import { CARD_HEIGHT, CARD_WIDTH } from '@/helpers/constants';
 
 export const PlayingCard = () => {
   const [frontTexture, backTexture] = useTexture([
     'textures/kenney/cardClubsA.png',
     'textures/kenney/cardBack_red2.png',
   ]);
-  const height = 2;
-  const width = height * ASPECT_RATIO;
-  const radius = width * 0.05;
+
+  const radius = CARD_WIDTH * 0.05;
   return (
     <>
       <object3D>
         <object3D>
-          <RoundedRect width={width} height={height} radius={radius}>
+          <RoundedRect width={CARD_WIDTH} height={CARD_HEIGHT} radius={radius}>
             <meshBasicMaterial map={frontTexture} />
           </RoundedRect>
         </object3D>
         <object3D rotation-y={Math.PI}>
-          <RoundedRect width={width} height={height} radius={radius}>
+          <RoundedRect width={CARD_WIDTH} height={CARD_HEIGHT} radius={radius}>
             <meshBasicMaterial map={backTexture} />
           </RoundedRect>
         </object3D>
---
+++ src/helpers/constants.ts
@@ -0,0 +1,3 @@
+export const CARD_WIDTH = 63.5e-3; // 63.5mm
+export const CARD_HEIGHT = 88.9e-3; // 88.9mm
+export const CARD_ASPECT_RATIO = CARD_WIDTH / CARD_HEIGHT;
