bf0c7e5b186ebcc469f6eeb72a9f48fc3f34e9ed
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-18 17:18:38 -0700

    add custom playing card shader

--- src/components/canvas/playing-card/PlayingCard.tsx
+++ src/components/canvas/playing-card/PlayingCard.tsx
@@ -1,30 +1,19 @@
 import { useTexture } from '@react-three/drei';
 import { RoundedRect } from '../RoundedRect';
 import { CARD_HEIGHT, CARD_WIDTH } from '@/helpers/constants';
+import { PlayingCardMaterial } from './playing-card-shader/PlayingCardMaterial';
 
 const Z_OFFSET = 1e-4;
 
 export const PlayingCard = () => {
-  const [frontTexture, backTexture] = useTexture([
-    'textures/kenney/cardClubsA.png',
-    'textures/kenney/cardBack_red2.png',
-  ]);
+  const frontTexture = useTexture('textures/kenney/cardClubsA.png');
 
   const radius = CARD_WIDTH * 0.05;
   return (
     <>
-      <object3D>
-        <object3D>
-          <RoundedRect width={CARD_WIDTH} height={CARD_HEIGHT} radius={radius}>
-            <meshLambertMaterial map={frontTexture} />
-          </RoundedRect>
-        </object3D>
-        <object3D rotation-y={Math.PI} position-z={Z_OFFSET}>
-          <RoundedRect width={CARD_WIDTH} height={CARD_HEIGHT} radius={radius}>
-            <meshLambertMaterial map={backTexture} />
-          </RoundedRect>
-        </object3D>
-      </object3D>
+      <RoundedRect width={CARD_WIDTH} height={CARD_HEIGHT} radius={radius}>
+        <PlayingCardMaterial frontTexture={frontTexture} />
+      </RoundedRect>
     </>
   );
 };
---
+++ src/components/canvas/playing-card/playing-card-shader/PlayingCardMaterial.tsx
@@ -0,0 +1,58 @@
+import { MaterialNode, extend, useFrame } from '@react-three/fiber';
+import { shaderMaterial, useTexture } from '@react-three/drei';
+import vertex from './playing-card-shader.vert';
+import fragment from './playing-card-shader.frag';
+import { Texture, ShaderMaterial, DoubleSide } from 'three';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+
+const PlayingCardShader = shaderMaterial(
+  {
+    frontTexture: new Texture(),
+    backTexture: new Texture(),
+  },
+  vertex,
+  fragment,
+);
+
+type PlayingCardShaderType = ShaderMaterial &
+  JSX.IntrinsicElements['shaderMaterial'] & {
+    frontTexture: Texture;
+    backTexture: Texture;
+  };
+
+extend({ PlayingCardShader });
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    playingCardShader: MaterialNode<
+      PlayingCardShaderType,
+      typeof PlayingCardShader
+    >;
+  }
+}
+
+export type PlayingCardMaterialProps = {
+  frontTexture: Texture;
+};
+const PlayingCardMaterial = forwardRef<
+  PlayingCardShaderType,
+  PlayingCardMaterialProps
+>(({ frontTexture }: PlayingCardMaterialProps, ref) => {
+  const backTexture = useTexture('textures/kenney/cardBack_red2.png');
+
+  const localRef = useRef<PlayingCardShaderType>(null!);
+  useImperativeHandle(ref, () => localRef.current);
+
+  return (
+    <playingCardShader
+      ref={localRef}
+      key={PlayingCardShader.key}
+      side={DoubleSide}
+      frontTexture={frontTexture}
+      backTexture={backTexture}
+      attach={'material'}
+    />
+  );
+});
+PlayingCardMaterial.displayName = 'PlayingCardMaterial';
+export { PlayingCardMaterial };
