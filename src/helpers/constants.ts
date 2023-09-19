6949d04ebbb5eed1adf2dc4038fc81e8e8de574f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-18 20:41:55 -0700

    add PILE_X_OFFSET

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,6 +1,9 @@
 export const CARD_WIDTH = 63.5e-3; // 63.5mm
 export const CARD_HEIGHT = 88.9e-3; // 88.9mm
+export const CARD_WIDTH_HALF = CARD_WIDTH / 2;
+export const CARD_HEIGHT_HALF = CARD_HEIGHT / 2;
 export const CARD_ASPECT_RATIO = CARD_WIDTH / CARD_HEIGHT;
+export const PILE_X_OFFSET = CARD_WIDTH_HALF + CARD_WIDTH * 0.05; // X-offset between piles.
 
 export const BACKGROUND_COLOR = '#03C03C';
 
