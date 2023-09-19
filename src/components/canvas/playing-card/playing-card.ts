15e11cc6c421c26cf7c630c655fb014d09d70227
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-18 20:26:40 -0700

    add class implementation for PlayingCard

--- src/components/canvas/playing-card/playing-card.ts
+++ src/components/canvas/playing-card/playing-card.ts
@@ -1,14 +1,23 @@
 import { makePlayingCardName } from '@/helpers/playing-card-utils';
 import { Object3D } from 'three';
+import { PileImpl } from '../piles/pile';
 
 export class PlayingCardImpl extends Object3D {
   private _rank: number;
   private _suit: number;
+
+  private _previousPile: PileImpl;
+  private _currentPile: PileImpl;
 
   constructor(rank: number, suit: number) {
     super();
     this._rank = rank;
     this._suit = suit;
     this.name = makePlayingCardName(rank, suit);
+  }
+
+  addToPile(pile: PileImpl) {
+    this._previousPile = this._currentPile;
+    this._currentPile = pile;
   }
 }
