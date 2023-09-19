d330e1ef2fa05ff5b703de1c19ef12c457664a32
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-18 20:58:27 -0700

    make vectors const

--- src/components/canvas/piles/StockAndWaste.tsx
+++ src/components/canvas/piles/StockAndWaste.tsx
@@ -5,15 +5,14 @@ import { Vector3 } from 'three';
 import { CARD_WIDTH_HALF, PILE_X_OFFSET } from '@/helpers/constants';
 import { PositionProps } from '@/helpers/props';
 
+const STOCK_POS = new Vector3(-PILE_X_OFFSET, 0, 0);
+const WASTE_POS = new Vector3(PILE_X_OFFSET, 0, 0);
+
 export const StockAndWaste = ({ position }: PositionProps) => {
-  const [stockPos, wastePos] = useMemo(
-    () => [new Vector3(-PILE_X_OFFSET, 0, 0), new Vector3(PILE_X_OFFSET, 0, 0)],
-    [],
-  );
   return (
     <group position={position}>
-      <StockPile position={stockPos} />
-      <WastePile position={wastePos} />
+      <StockPile position={STOCK_POS} />
+      <WastePile position={WASTE_POS} />
     </group>
   );
 };
