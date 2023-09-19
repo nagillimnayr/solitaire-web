edb88f52367891e1e08762e493d925daa7dbb9b7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-09-19 16:22:55 -0700

    subscribe to game actor and add cards to stock pile on RESTART event

--- src/components/canvas/playing-card/PlayingCard.tsx
+++ src/components/canvas/playing-card/PlayingCard.tsx
@@ -2,10 +2,18 @@ import { RoundedRect } from '../RoundedRect';
 import { CARD_HEIGHT, CARD_WIDTH, RANKS, SUITS } from '@/helpers/constants';
 import { PlayingCardMaterial } from './playing-card-shader/PlayingCardMaterial';
 import { usePlayingCardTexture } from './usePlayingCardTexture';
-import { forwardRef, useImperativeHandle, useMemo, useRef } from 'react';
+import {
+  forwardRef,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+} from 'react';
 import { makePlayingCardName } from '@/helpers/playing-card-utils';
 import { Object3DNode, extend } from '@react-three/fiber';
 import { PlayingCardImpl } from './PlayingCardImpl';
+import { GlobalStateContext } from '@/components/dom/providers/GlobalStateProvider';
 
 extend({ PlayingCardImpl });
 declare module '@react-three/fiber' {
@@ -15,7 +23,9 @@ declare module '@react-three/fiber' {
 }
 
 const START_X = -(RANKS.length * CARD_WIDTH) / 2;
-const START_Y = (SUITS.length * CARD_HEIGHT) / 2;
+const START_Y = (Object.keys(SUITS).length * CARD_HEIGHT) / 2;
+
+const RADIUS = CARD_WIDTH * 0.04;
 
 type PlayingCardProps = {
   rank: number;
@@ -23,6 +33,8 @@ type PlayingCardProps = {
 };
 const PlayingCard = forwardRef<PlayingCardImpl, PlayingCardProps>(
   ({ rank, suit }: PlayingCardProps, ref) => {
+    const { GameActor } = useContext(GlobalStateContext);
+
     const frontTexture = usePlayingCardTexture(rank, suit);
 
     const localRef = useRef<PlayingCardImpl>(null!);
@@ -32,7 +44,18 @@ const PlayingCard = forwardRef<PlayingCardImpl, PlayingCardProps>(
     const args: [number, number] = useMemo(() => [rank, suit], [rank, suit]);
     const name = makePlayingCardName(rank, suit);
 
-    const radius = CARD_WIDTH * 0.04;
+    useEffect(() => {
+      const subscription = GameActor.subscribe((state) => {
+        if (state.event.type !== 'RESTART') return;
+        const card = localRef.current;
+        const { stockPile } = state.context;
+        card.addToPile(stockPile);
+      });
+
+      // Cleanup.
+      return () => subscription.unsubscribe();
+    }, [GameActor]);
+
     return (
       <>
         <playingCardImpl
@@ -43,7 +66,7 @@ const PlayingCard = forwardRef<PlayingCardImpl, PlayingCardProps>(
           name={name}
           userData={metaData}
         >
-          <RoundedRect width={CARD_WIDTH} height={CARD_HEIGHT} radius={radius}>
+          <RoundedRect width={CARD_WIDTH} height={CARD_HEIGHT} radius={RADIUS}>
             <PlayingCardMaterial frontTexture={frontTexture} />
           </RoundedRect>
         </playingCardImpl>
