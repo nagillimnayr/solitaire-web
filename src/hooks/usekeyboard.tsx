4b2f78b85a7d06b2413a73629c91273e490eac84
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-22 14:42:44 -0700

    disable console log

--- src/hooks/usekeyboard.tsx
+++ src/hooks/usekeyboard.tsx
@@ -1,13 +1,19 @@
 import { GlobalStateContext } from '@/components/dom/providers/GlobalStateProvider';
+import { CameraControls } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
 import { useContext } from 'react';
+import { radToDeg } from 'three/src/math/MathUtils';
 import { useEventListener } from 'usehooks-ts';
 
 export function useKeyboard() {
   const { GameActor } = useContext(GlobalStateContext);
+
+  const getThree = useThree(({ get }) => get);
+
   useEventListener('keypress', (event) => {
     event.preventDefault();
     event.stopPropagation();
-    console.log(event.code);
+    // console.log(event.code);
     switch (event.code) {
       case 'Space': {
         GameActor.send({ type: 'DRAW_CARD' });
@@ -41,6 +47,16 @@ export function useKeyboard() {
         }
         break;
       }
+      // case 'KeyI': {
+      //   const controls = getThree().controls as unknown as CameraControls;
+      //   if (!controls) return;
+
+      //   console.log(`azimuthAngle: ${radToDeg(controls.azimuthAngle)}`);
+      //   console.log(`polarAngle: ${radToDeg(controls.polarAngle)}`);
+      //   console.log(`distance: ${controls.distance}`);
+
+      //   break;
+      // }
     }
   });
 }
