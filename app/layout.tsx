317b192300a355e2910fcb4292d69bc059e9d270
Ryan Milligan <ryanmilligan88@gmail.com>
2023-10-31 18:49:13 -0700

    add Providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Layout } from '@/components/dom/Layout';
 import { GlobalStateProvider } from '@/components/dom/providers/GlobalStateProvider';
+import { Providers } from '@/components/dom/providers/Providers';
 import '@/global.css';
 
 export const metadata = {
@@ -17,9 +18,9 @@ export default function RootLayout({ children }) {
       <head />
       <body>
         {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
-        <GlobalStateProvider>
+        <Providers>
           <Layout>{children}</Layout>
-        </GlobalStateProvider>
+        </Providers>
       </body>
     </html>
   );
